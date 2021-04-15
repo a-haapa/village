2ef9a2cc94d7cbed9a021a214b9a97f324013d2b
a-haapa <82581773+a-haapa@users.noreply.github.com>
2021-04-15 20:52:24 +0300

    korjaus tietokantaan

--- village/Properties/Settings.Designer.cs
+++ village/Properties/Settings.Designer.cs
@@ -25,7 +25,7 @@ namespace village.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("C:\\village\\a-haapa\\village")]
+        [global::System.Configuration.DefaultSettingValueAttribute("C:\\village\\a-haapa\\village\\villageDB")]
         public string database {
             get {
                 return ((string)(this["database"]));
