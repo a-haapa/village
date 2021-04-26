875c4e3b0adbf439c00f5e42ebda6ac2abcbb3ca
a-haapa <82581773+a-haapa@users.noreply.github.com>
2021-04-26 11:16:01 +0300

    Lisätty luokat toiminta-alue ja palvelu

---
+++ village/Palvelu.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace village
+{
+    class Palvelu
+    {
+        public int Palvelu_id { get; set; }
+        public string Nimi { get; set; }
+        public string Kuvaus { get; set; }
+        public double Hinta { get; set; }
+        public double Alv { get; set; }
+        public override string ToString()
+        {
+            return Palvelu_id + Nimi + Kuvaus + Hinta + Alv;
+        }
+    }
+}
---
+++ village/Toimintaalue.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace village
+{
+    class Toimintaalue
+    {
+        public int Toimintaalue_id { get; set; }
+        public string Nimi { get; set; }
+
+        public override string ToString()
+        {
+            return Toimintaalue_id + Nimi;
+        }
+    }
+}
