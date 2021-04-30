dae58df6baf8603e1802d80d415e461cdf6687f5
Tomi Eskelinen <Tomi.S.Eskelinen@edu.savonia.fi>
2021-04-30 11:50:14 +0300

    Palvelun muokkausta työstetty

--- village/Muokkaa_palvelua.Designer.cs
+++ village/Muokkaa_palvelua.Designer.cs
@@ -36,12 +36,14 @@ namespace village
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.tbPalveluNimi = new System.Windows.Forms.TextBox();
+            this.tbTyyppi = new System.Windows.Forms.TextBox();
+            this.tbKuvaus = new System.Windows.Forms.TextBox();
+            this.tbHinta = new System.Windows.Forms.TextBox();
+            this.tbAlv = new System.Windows.Forms.TextBox();
+            this.cbToimintaAlue = new System.Windows.Forms.ComboBox();
+            this.btnTallenna = new System.Windows.Forms.Button();
+            this.btnPoistu = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -57,7 +59,7 @@ namespace village
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(62, 174);
+            this.label2.Location = new System.Drawing.Point(80, 186);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(76, 25);
             this.label2.TabIndex = 1;
@@ -66,7 +68,7 @@ namespace village
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(62, 245);
+            this.label3.Location = new System.Drawing.Point(80, 257);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(131, 25);
             this.label3.TabIndex = 2;
@@ -75,7 +77,7 @@ namespace village
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(62, 313);
+            this.label4.Location = new System.Drawing.Point(80, 325);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(71, 25);
             this.label4.TabIndex = 3;
@@ -84,7 +86,7 @@ namespace village
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(62, 382);
+            this.label5.Location = new System.Drawing.Point(80, 394);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(79, 25);
             this.label5.TabIndex = 4;
@@ -93,7 +95,7 @@ namespace village
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(62, 453);
+            this.label6.Location = new System.Drawing.Point(80, 465);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(57, 25);
             this.label6.TabIndex = 5;
@@ -102,66 +104,88 @@ namespace village
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(62, 516);
+            this.label7.Location = new System.Drawing.Point(80, 528);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(58, 25);
             this.label7.TabIndex = 6;
             this.label7.Text = "Alv%";
             // 
-            // textBox1
+            // tbPalveluNimi
             // 
-            this.textBox1.Location = new System.Drawing.Point(259, 170);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(204, 29);
-            this.textBox1.TabIndex = 7;
+            this.tbPalveluNimi.Location = new System.Drawing.Point(277, 182);
+            this.tbPalveluNimi.Name = "tbPalveluNimi";
+            this.tbPalveluNimi.Size = new System.Drawing.Size(204, 29);
+            this.tbPalveluNimi.TabIndex = 7;
             // 
-            // textBox2
+            // tbTyyppi
             // 
-            this.textBox2.Location = new System.Drawing.Point(259, 310);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(204, 29);
-            this.textBox2.TabIndex = 8;
+            this.tbTyyppi.Location = new System.Drawing.Point(277, 322);
+            this.tbTyyppi.Name = "tbTyyppi";
+            this.tbTyyppi.Size = new System.Drawing.Size(204, 29);
+            this.tbTyyppi.TabIndex = 8;
             // 
-            // textBox3
+            // tbKuvaus
             // 
-            this.textBox3.Location = new System.Drawing.Point(259, 378);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(204, 29);
-            this.textBox3.TabIndex = 9;
+            this.tbKuvaus.Location = new System.Drawing.Point(277, 390);
+            this.tbKuvaus.Name = "tbKuvaus";
+            this.tbKuvaus.Size = new System.Drawing.Size(204, 29);
+            this.tbKuvaus.TabIndex = 9;
             // 
-            // textBox4
+            // tbHinta
             // 
-            this.textBox4.Location = new System.Drawing.Point(259, 449);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(204, 29);
-            this.textBox4.TabIndex = 10;
+            this.tbHinta.Location = new System.Drawing.Point(277, 461);
+            this.tbHinta.Name = "tbHinta";
+            this.tbHinta.Size = new System.Drawing.Size(204, 29);
+            this.tbHinta.TabIndex = 10;
             // 
-            // textBox5
+            // tbAlv
             // 
-            this.textBox5.Location = new System.Drawing.Point(259, 512);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(204, 29);
-            this.textBox5.TabIndex = 11;
+            this.tbAlv.Location = new System.Drawing.Point(277, 524);
+            this.tbAlv.Name = "tbAlv";
+            this.tbAlv.Size = new System.Drawing.Size(204, 29);
+            this.tbAlv.TabIndex = 11;
             // 
-            // comboBox1
+            // cbToimintaAlue
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(259, 236);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(204, 32);
-            this.comboBox1.TabIndex = 12;
+            this.cbToimintaAlue.FormattingEnabled = true;
+            this.cbToimintaAlue.Location = new System.Drawing.Point(277, 248);
+            this.cbToimintaAlue.Name = "cbToimintaAlue";
+            this.cbToimintaAlue.Size = new System.Drawing.Size(204, 32);
+            this.cbToimintaAlue.TabIndex = 12;
+            // 
+            // btnTallenna
+            // 
+            this.btnTallenna.Location = new System.Drawing.Point(85, 644);
+            this.btnTallenna.Name = "btnTallenna";
+            this.btnTallenna.Size = new System.Drawing.Size(148, 43);
+            this.btnTallenna.TabIndex = 13;
+            this.btnTallenna.Text = "Tallenna";
+            this.btnTallenna.UseVisualStyleBackColor = true;
+            this.btnTallenna.Click += new System.EventHandler(this.btnTallenna_Click);
+            // 
+            // btnPoistu
+            // 
+            this.btnPoistu.Location = new System.Drawing.Point(324, 644);
+            this.btnPoistu.Name = "btnPoistu";
+            this.btnPoistu.Size = new System.Drawing.Size(148, 43);
+            this.btnPoistu.TabIndex = 14;
+            this.btnPoistu.Text = "Poistu";
+            this.btnPoistu.UseVisualStyleBackColor = true;
+            this.btnPoistu.Click += new System.EventHandler(this.btnPoistu_Click);
             // 
             // Muokkaa_palvelua
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(662, 792);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.textBox5);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.ClientSize = new System.Drawing.Size(578, 799);
+            this.Controls.Add(this.btnPoistu);
+            this.Controls.Add(this.btnTallenna);
+            this.Controls.Add(this.cbToimintaAlue);
+            this.Controls.Add(this.tbAlv);
+            this.Controls.Add(this.tbHinta);
+            this.Controls.Add(this.tbKuvaus);
+            this.Controls.Add(this.tbTyyppi);
+            this.Controls.Add(this.tbPalveluNimi);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
@@ -185,11 +209,13 @@ namespace village
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox5;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox tbPalveluNimi;
+        private System.Windows.Forms.TextBox tbTyyppi;
+        private System.Windows.Forms.TextBox tbKuvaus;
+        private System.Windows.Forms.TextBox tbHinta;
+        private System.Windows.Forms.TextBox tbAlv;
+        private System.Windows.Forms.ComboBox cbToimintaAlue;
+        private System.Windows.Forms.Button btnTallenna;
+        private System.Windows.Forms.Button btnPoistu;
     }
 }
