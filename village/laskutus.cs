5e4ec5cab363dd347d84977d4048447c3b695cee
a-haapa <82581773+a-haapa@users.noreply.github.com>
2021-05-07 12:36:22 +0300

    laskutusta

---
+++ village/laskutus.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace village
+{
+    public partial class laskutus : Form
+    {
+        public laskutus()
+        {
+            InitializeComponent();
+            dtpAlku.CustomFormat = " ";
+            dtpLoppu.CustomFormat = " ";
+        }
+
+        private void dtpAlku_ValueChanged(object sender, EventArgs e)
+        {
+
+            dtpAlku.CustomFormat = "dd/MM/yyyy hh:mm:ss";
+        }
+
+        private void dtpLoppu_ValueChanged(object sender, EventArgs e)
+        {
+
+            dtpLoppu.CustomFormat = "dd/MM/yyyy hh:mm:ss";
+        }
+    }
+}
